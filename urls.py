c0911c259abf96ddefe8b1afe8f1623eb73fcf0f
John Grinwis <john.grinwis@gmail.com>
2021-11-17 20:06:37 +0100

    added some extra comments and code cleanup

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
-from myakamai import MyAkamai
 import pandas as pd
+from myakamai import MyAkamai
 
 
 if __name__ == "__main__":
@@ -17,6 +17,7 @@ if __name__ == "__main__":
 
     # now let's load our list as a panda's dataframe and set the correc decimals.
     # the from_dict is setting every column to an object(df.info()) so we need to fix that.
+    # there should be a better way of setting the types per column.
     df = pd.DataFrame.from_dict(reporting.get_urls_by_cpcode(cpcodes))
     df = df.astype({"allEdgeBytes": int})
     df = df.astype({"allOriginBytes": int})
@@ -39,6 +40,11 @@ if __name__ == "__main__":
     output.to_csv(f"results/urls-{reporting.end}.csv", index=False)
 
     print(
-        f"top 5 files with low offload. The complete list can be found here: results/{reporting.end}.csv\n"
+        f"Below a list with the top 5 files with a low offload.\n"
+        f"The complete list can be found here: results/{reporting.end}.csv\n"
     )
+
     print(output.head(5))
+
+    # next up we can do a request for a couple of file to check the cache-control header
+    # to be continued....
